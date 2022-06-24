949b1308385e908b3dcbb489cd5a2433d2c0bba3
Abdinasir Yussuf <Abdinasir.yussuf@outlook.com>
2022-06-24 13:27:56 -0700

    all files without node modules

--- src/auth/models/index.js
+++ src/auth/models/index.js
@@ -1,13 +1,14 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
-const userSchema = require('./user');
-const UserBoardSchema = require('./userBoard');
+const userSchema = require('./message-board-user');
+const msgBoardSchema = require('./userBoard');
 
 
 
-const DATABASE_URL = 'sqlite::memory';
 
-const sequelize = new Sequelize(DATABASE_URL, {
+const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite::memory' : process.env.DATABASE_URL;
+
+const DATABASE_CONFIG = new Sequelize(DATABASE_URL, {
   dialectOptions: {
     ssl: {
       require: true,
@@ -16,8 +17,13 @@ const sequelize = new Sequelize(DATABASE_URL, {
   },
 });
 
+const sequelize = new Sequelize(DATABASE_URL, DATABASE_CONFIG);
+
+const msgBoardModel = new msgBoardSchema(sequelize, DataTypes);
+
+
 module.exports = {
   sequelize: db,
-  personInterface: new modelInterface(personModel),
-  foodInterface: new modelInterface(foodModel),
+  users: userSchema(sequelize,DataTypes),
+  msgBoardModel,
 };
