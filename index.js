6267e866e7a490d7f3778ccb1c0aa4627184d10f
DYLAN ULLRICH <DYLAN@GETULLRICHORDIETRYING.COM>
2022-06-27 07:16:35 -0700

    fixed config

--- index.js
+++ index.js
@@ -4,7 +4,5 @@ const { start } = require('./src/server');
 const { db } = require('./src/auth/models');
 
 db.sync().then(() => {
-    start(process.env.PORT || 3001);
+  start(process.env.PORT || 3001);
 });
-
-
